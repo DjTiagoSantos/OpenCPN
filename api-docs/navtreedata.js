ada1c122fdc43404fe1db731b2adac6fcb12ad6f
nohal <pavel@kalian.cz>
2024-02-17 03:33:23 +0000

    [CI] Updating gh-pages branch from 703f57ef

---
+++ api-docs/navtreedata.js
@@ -0,0 +1,111 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "OpenCPN Partial API docs", "index.html", [
+    [ "Communications Overview", "comm.html", null ],
+    [ "Handling Communications Messages in Plugins", "plugincomms.html", null ],
+    [ "wxWidgets Observable Framework", "observable.html", [
+      [ "Introduction", "observable.html#Introduction", null ],
+      [ "Why", "observable.html#Why", null ],
+      [ "Extending", "observable.html#Extending", null ],
+      [ "Copyright", "observable.html#Copyright", null ]
+    ] ],
+    [ "Namespaces", "namespaces.html", [
+      [ "Namespace List", "namespaces.html", "namespaces_dup" ],
+      [ "Namespace Members", "namespacemembers.html", [
+        [ "All", "namespacemembers.html", null ],
+        [ "Functions", "namespacemembers_func.html", null ]
+      ] ]
+    ] ],
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Index", "classes.html", null ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", "functions_func" ],
+        [ "Variables", "functions_vars.html", null ],
+        [ "Related Functions", "functions_rela.html", null ]
+      ] ]
+    ] ],
+    [ "Files", "files.html", [
+      [ "File List", "files.html", "files_dup" ],
+      [ "File Members", "globals.html", [
+        [ "All", "globals.html", null ],
+        [ "Functions", "globals_func.html", null ],
+        [ "Typedefs", "globals_type.html", null ],
+        [ "Enumerations", "globals_enum.html", null ],
+        [ "Enumerator", "globals_eval.html", null ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"AISTargetAlertDialog_8cpp_source.html",
+"certificates_8cpp_source.html",
+"classAisData.html#a3f167011c08e3886fea06eba17c7f8e8",
+"classCandidateButtonsPanel.html",
+"classChartBaseBSB.html#a5978ba4f84e3f4c492a3acaae7c64313",
+"classChartCanvas.html#a8f184339253fdd8b21bcd9ad1e658f35",
+"classChartDirPanelHardBreakWrapper.html#a3a72ec6d4008ea43b4e7ce08434508be",
+"classCommDriverN0183AndroidBT.html#a82de5e296d2dd8f2f8aac50885968c5f",
+"classConfigPanel.html#a17201e16cd1a3675f251b8be9e6bf30a",
+"classCustomCatalogCtrl.html#ae00ecf399493d7814e429c2eca4fe895",
+"classGoToPositionDialog.html#ad4b5345b7ec93ecbf0306d1032684a99",
+"classLayer.html#a501f60f82f5aadac5fffdba6cab79fa8",
+"classMarkInfoDlg.html#a750886a8a233c7ef7e70e090c1b67419",
+"classMyFrame.html#a6dc49068ecfeaa4362a841612cbdcc90",
+"classNmeaLog.html#a0aafb30705f1ebbe7a0ab0117ca4d1b0",
+"classOCPN__BUILDSENC__ThreadEvent.html#aeb5e07a4f4bccad5827f0d5d21542586",
+"classPI__S57Obj.html#a243eba8689f9d3385c34725e8462cc89",
+"classPlugInData.html#a7858573baf92f41a9880991a65f4a8a0",
+"classPluginIconPanel.html#a0eca760a5f113f7bd58be90c06426a7a",
+"classQuilt.html#ad09418f963ed2c210c85143c798d5f86",
+"classRoutePoint.html#a7146e0591c04e16e96960baedcaa4a27",
+"classRouteman.html#a769a58a002cedc3c3251a62d035b23a0",
+"classSendToPeerDlg.html#aa34708f3b55fe9d1e52bd9d2773213b7",
+"classToolbarItemContainer.html#a240bbb2bcd927976115b7e316d2991df",
+"classViewPort.html#a9a1b76ed2d7357449e36157e57f6b357",
+"classcm93compchart.html#a10c29eb072fcb321f9ab5bcb2567f844",
+"classglChartCanvas.html#aeaf670218e116527a00c82764c2fd3f9",
+"classocpnFloatingToolbarDialog.html#a572f971744f51c12ae5c5c7aea9f9d0a",
+"classocpnToolBarSimple.html#a844b5ae506ecc2bda2051f664d640017",
+"classoptions.html#a2e5909c21c152c865d246fae149ea0a0",
+"classoptions.html#aec12ac69342e140a811b2b01699a30da",
+"functions.html",
+"ocpn__plugin_8h.html#a2b4f74207aacba0b4ac86e59245632c8",
+"ocpn__plugin_8h.html#accac8eec1210b7be80924c4dfc4f4070",
+"structButtons.html#aeef2d6c8fb85dc4bf318d2c58692d42a",
+"structDDFIndexedRecord.html",
+"structSemanticVersion.html#a44e20817948b5c7d9b94885334c26a44",
+"struct__OSENC__EXTENT__Record__Payload.html#aec2ce1ea32cb22d4796e321b2f4ff636",
+"structocpn__DNS__record__t.html#a34e7d813c0d75ffa26836220d18e0472"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
